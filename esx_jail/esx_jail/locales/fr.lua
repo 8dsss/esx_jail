71a0a3cedf5e0a360ed712d32ced8a7b2ce1b5cf
8dsss <94976902+8dsss@users.noreply.github.com>
2022-03-07 15:04:53 +0100

    Delete esx_jail directory

--- esx_jail/esx_jail/locales/fr.lua
+++
@@ -1,8 +0,0 @@
-Locales ['fr'] = {
-  ['blip_name'] = 'Pénitencier de IrisRP',
-  ['judge'] = 'Juge',
-  ['escape_attempt'] = 'vous n\'êtes pas autorisé à vous échapper de la prison !',
-  ['remaining_msg'] = 'il reste ~b~%s~s~ secondes avant votre sortie de prison',
-  ['jailed_msg'] = '%s est maintenant en prison pour %s minutes',
-  ['unjailed'] = '%s a été libéré de prison !'
-}
